d166b9436dd7360f0f8d23ee21c7f5ddc5ef9e8a
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-06-08 14:23:36 +0530

    somechanges imports a files in app.jsx and images in assets

--- My-portfolio/src/App.jsx
+++ My-portfolio/src/App.jsx
@@ -1,11 +1,34 @@
-import React from 'react'
+import Navbar from "./components/Navbar";
+import Home from "./section/Home";
+import About from "./section/About";
+import Skills from "./section/Skills";
+import Project from "./section/Project";
+import Experienace from "./section/Experienace";
+import Testimonials from "./section/Testimonials";
+import Contact from "./section/Contact";
+import Footer from "./section/Footer";
+import ParticlesBackground from "./components/ParticlesBackground";
 
-function App() {
-  return (
-    <div>
-      
+ 
+
+
+
+ export default function App(){
+  return(
+    <div className="relative gradient text-white">
+      <particlesBackground/>
+      <Navbar/>
+      <Home/>
+      <About/>
+      <Skills/>
+      <Project/>
+      <Experienace/>
+      <Testimonials/>
+      <Contact/>
+      <Footer/>
     </div>
   )
-}
 
-export default App
+ }
+
+
---
+++ My-portfolio/src/section/Experienace.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Experienace = () => {
+  return (
+    <div className=' w-full h-screen'>
+    Experience
+    </div>
+  )
+}
+
+export default Experienace
