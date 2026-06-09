38a2aa11f260501abe92283c7dca07330a70d2b6
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-06-09 21:12:41 +0530

    chnages of practicleBackground

--- My-portfolio/src/components/ParticlesBackground.jsx
+++ My-portfolio/src/components/ParticlesBackground.jsx
@@ -1,11 +1,21 @@
-import React from 'react'
+import { useEffect, useRef } from "react";
 
-const ParticlesBackground = () => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+export default function ParticlesBackground() {
+  const canvasRef = useRef(null);
+  useEffect(() => {
+    const canvas = canvasRef.currentRef.current;
+    const ctx = canvas.getContext("2d");
+    let particles = [];
+    const particleCount = 50;
+    const colors = ["rgbd (255,255,255,0.7)"];
+
+    class Particle {
+      constructor(){
+        this.x = Math.random()*canvas.width;
+        this.y = Math.random()*canvas.height;
+        this.radius = Math.random()*2+1;
+      }
+    }
+  })
 }
-
-export default ParticlesBackground
