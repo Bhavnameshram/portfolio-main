dfc39e1ef0a9021c12784ef92de3ede669d00c7c
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-06-12 12:44:14 +0530

    make a react icons and logo import img and button

--- My-portfolio/src/components/Navbar.jsx
+++ My-portfolio/src/components/Navbar.jsx
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import OverlayMenu from "./OverlayMenu";
+import Logo from "../assets/Logo.png"
+import { FiMenu } from "react-icons/fi";
 
 export default function Navbar() {
   const [menuOpen, setMenuOpen] = useState(false);
@@ -12,6 +14,25 @@ export default function Navbar() {
           visible ? "translate-y-0" : "-translate-y-full"
         }`}
       >
+        <div className=" flex items-center space-x-2">
+          <img src={Logo} alt="logo" className="w-8 h-8"/>
+          <div className="text-2xl font-bold text-white hidden sm:block">
+            Bhavna
+          </div>
+        </div>
+        <div className="block lg:absolute lg:left-1/2 lg:transform lg:translate-x-1/2">
+        <button onClick={() => setMenuOpen(true)}
+          className= "text-white text-3xl focus:outline-none" aria-label="open Menu">
+            <FiMenu />
+            </button>
+          </div>
+          <div className="hidden lg:block">
+            <a href="#constact"
+            className="bg-gradient-to-r from-pink-500 to-blue-500 text-white px-5 py-2 rounded-full font-medium shadow-lg hover:opacity duration-300">
+              Reach Out
+            </a>
+
+          </div>
       </nav>
 
       <OverlayMenu />
