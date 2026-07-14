1f1eb65e9336905bc989c31f55f1a45ccc7fcb0e
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-07-14 14:53:44 +0530

    add a paragraph make a button and import icons  with motion

--- My-portfolio/src/section/Home.jsx
+++ My-portfolio/src/section/Home.jsx
@@ -2,6 +2,27 @@ import { useMemo } from "react"
 import ParticlesBackground from "../components/ParticlesBackground"
 import { motion } from "framer-motion"
 import React from "react"
+import { FaGithub, FaLinkedin } from "react-icons/fa";
+import { FaXTwitter } from "react-icons/fa6";
+
+const socials = [
+  { Icon: FaGithub, label: "GitHub", href: "href:/" },
+  { Icon: FaLinkedin, label: "Linkeddin", href: "href:/" },
+  { Icon: FaXTwitter, label: "X", href: "href:/" }
+]
+
+const glowVarients = {
+  initial: {scale :1, y: 0 , filter : "drop-shadow(0 0 0 rgba (0,0,0,0))"},
+  hover: {
+    scale:1.2, y:-3,
+    filter : "drop-shadow (0 0 8px rgba(13,88,204,0.9)) drop-shadow(0 0 10px rgba(16, 185, 129,0.8))",
+    transition :{type : "spring ", stiffness: 300 , damping:15}
+
+  },
+  tap : {scale :0.95 ,y:0, transition: {duration:0.88}}
+}
+
+
 
 export default function Home() {
   const roles = useMemo(() => ["Web Developer", "MernStackDeveloper"], [])
@@ -26,7 +47,7 @@ export default function Home() {
         setDeleting(false);
         setIndex(p => (p + 1) % roles.length);
       }
-        
+
 
     }, deleting ? 40 : 60);
     return () => clearTimeout(timeout);
@@ -38,7 +59,7 @@ export default function Home() {
       <ParticlesBackground />
       <div className=" absolute inset-0">
         <div className=" absolute -top-32 -left-32
-  w-[70vw] sm:w-[z-500vw] md:w-[40vw]
+  w-[70vw] sm:w-[500vw] md:w-[40vw]
   h-[70vw] sm:h-[50vw] md:h-[40vw]
   max-w-[500px] max-h-[500px]
   rounded-full
@@ -77,21 +98,62 @@ export default function Home() {
               </span>
             </motion.div>
 
-         <motion.h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-transparent bg-clip-text
+            <motion.h1 className="text-4xl sm:text-5xl md:text-6xl lg:text-7xl font-bold text-transparent bg-clip-text
          bg-gradient-to-r from-[#1cd8d2] via-[#00bf8f] to-[#302b63] drop-shadow-lg"
-         initial={{opacity:0  ,y:40}}
-         animate={{opacity:1  ,y:0}}
-         transition={{duration:1}}
-         >
-          Hello I'm 
-          <br/>
-          <span className=" text-white font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl lg:whitespace-nowrap">
-            Bhavna Meshram  
-          </span>
-         </motion.h1>
+              initial={{ opacity: 0, y: 40 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ duration: 1 }}
+            >
+              Hello I'm
+              <br />
+              <span className=" text-white font-bold text-5xl sm:text-6xl md:text-7xl lg:text-8xl lg:whitespace-nowrap">
+                Bhavna Meshram
+              </span>
+            </motion.h1>
+            <motion.p className=" mt-6 text-base sm:text-lg md:text-xl text-gray-300 mx-w-2xl max-auto lg:mx-0"
+              initial={{ opacity: 0, y: 20 }}
+              animate={{ opacity: 1, y: 0 }}
+              transition={{ delay: 0.4, duration: 0.8 }}
 
 
+            >
 
+              I turn complex ides into seamless, high-impact web experience- buliding modern, and lighning-fast application that make a differecec
+
+            </motion.p>
+            <motion.div className="mt-10 flex flex-wrap items-center justify-center lg:justify-start gap-6"
+              initial={{ opacity: 0 }}
+              animate={{ opacity: 1 }}
+              transition={{ delay: 0.8, duration: 0.8 }}>
+              <a href="#project" className="px-6 py-3 rounded-full font-medium text-lg text-white 
+              bg-gradient-to-r from-[#1cd8d2] via-[#00bf8f] to-[#302b63]
+             shadow-lg hover:scale-105 transition-all "
+              > View My Work </a>
+              <a href="/Bhavna Meshram Tcs.pdf" download
+                className=" px-6 py-3 rounded-full text-lg font-medium text-black bg-white hover:bg-gray-200 shadow-lg hover:scale-105 transition-all
+    "> My Resume </a>
+            </motion.div>
+            <div className="mt-10 flex - gap-5 text-2xl md:text-3xl justify-center lg:justify-start">
+              {socials.map(({ Icon, label, href }) => (
+                <motion.a 
+                href={href}
+                key={label}
+                target="black" 
+                aria-label={label}
+                rel="noopener noreferrer"
+                varients={glowVarients} 
+                initial="initial"
+                whileHover="hover"
+                whileTap="tap"
+                className="text-gray-300" > 
+                 <Icon/>
+                </motion.a>
+
+       
+              )
+              )}
+
+            </div>
           </div>
         </div>
       </div>
