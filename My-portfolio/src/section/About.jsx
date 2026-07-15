896e122d160811c3149ae4913a984acc2049a3a5
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-07-15 09:19:32 +0530

    start a about Us page with glows make glows  3

--- My-portfolio/src/section/About.jsx
+++ My-portfolio/src/section/About.jsx
@@ -1,12 +1,19 @@
-import React from 'react'
 
-const About = () => {
+export default function About() {
+  const glows = [
+    "-top-10 -left-10 w-[360px] h-[360px] opacity-20 blur-[120px]",
+    "bottom-0 right-10 w-[420px] opacity-15 blur[140px] delay-300",
+    "top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w[220px] h-[220px] opacity-10 blur-[100px]",
+  ]
+
   return (
-    <div className=' w-full h-screen'>
-      About
-
-    </div>
+    <section id="about"
+      className="min-h-screen w-full items-center justify-center reaktive bg-black text-white overflow-hidden">
+      <div className="absolute inset-0 pointer-events-none ">
+        {glows.map((c, i) => (
+          <div key={i} className={"absolute rounded-full bg-gradient-to-r from-[3302bd3] via-[#00bf8f] to-[#1cd8d2] animate-pulse ${c}"} />
+        ))}
+      </div>
+    </section>
   )
 }
-
-export default About
