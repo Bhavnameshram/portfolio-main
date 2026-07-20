971f8cc035ef8485ce35a8c5348ef0bef87a01d2
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-07-20 17:02:09 +0530

    make a array to store error and 2nd one  status to store

--- My-portfolio/src/section/Contact.jsx
+++ My-portfolio/src/section/Contact.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import ParticlesBackground from "../components/ParticlesBackground"
 
 
@@ -15,6 +15,8 @@ export default function Contact(){
         budget:"",
         idea :"",
     });
+    const [errors, setErrors]= useEffect({});
+    const [status, setErrors]= useEffect("");
     return(
         <section id="contact" className="w-full min-h-screen relative bg-black overflow-hidden text-white py-20 px-6 md:px-20 flex flex-col
         md:flex-row items-center gap-10
