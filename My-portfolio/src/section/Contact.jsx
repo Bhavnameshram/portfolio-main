9589d47e85ff91bf5911ed9baf43d4e390ccd7ec
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-07-20 20:28:32 +0530

    creating from section

--- My-portfolio/src/section/Contact.jsx
+++ My-portfolio/src/section/Contact.jsx
@@ -1,76 +1,130 @@
 import { useEffect, useState } from "react";
 import ParticlesBackground from "../components/ParticlesBackground"
 import emailjs from "@emailjs/browser";
+import {motion} from "framer-motion";
+import Astra from "../assets/Astra (1).png"
 
 
 
 const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
 const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
 const PUBLIC_ID = import.meta.env.VITE_PUBLIC_KEY;
-export default function Contact(){
-    const [formdata , setformData]= useState({
-        name : "",
-        email :"",
-        service:"",
-        budget:"",
-        idea :"",
+export default function Contact() {
+    const [formdata, setformData] = useState({
+        name: "",
+        email: "",
+        service: "",
+        budget: "",
+        idea: "",
     });
-    
-    const [errors, setErrors]= useEffect({});
-    const [status, setStatus]= useEffect("");
 
-    const handleChange =(e)=>{
-        const { name , value} = e.target;
-        if(name === "budget" && !/^\d+$/.test(value)) return;
-        setFormData((p)=> ({...p, [name]: value}));
-        if(error[name]) setErrors((p)=>({...p , [name] :""}));
+    const [errors, setErrors] = useEffect({});
+    const [status, setStatus] = useEffect("");
+
+    const handleChange = (e) => {
+        const { name, value } = e.target;
+        if (name === "budget" && !/^\d+$/.test(value)) return;
+        setFormData((p) => ({ ...p, [name]: value }));
+        if (error[name]) setErrors((p) => ({ ...p, [name]: "" }));
     }
-const validateform =()=> {
-    const required = ["name", "email", "service","idea"];
-    const newErrors ={};
-    required.foreach ((f) => !formData[f].trim() && (newErrors[f] = "fill this field"));
-    if (formData.service !== "other" && !formData.budget.trim())
-        newErrors.budget ="fill this field";
-    setErrors(newErrors);
-    return !Object.keys(newErrors).length;
-}
-const handleSubmit = async(e) =>{
-    e.preventDefoult();
-    if (!validateForm()) return;
-    setStatus("sending");
-}
-try{
-    await emailjs.send(
-        SEVICE_ID,
-        TEMPLATE_ID,
-        {
-            ...formData,
-            from_name:formData.name,
-            reply_to: formData.email,
-        },
-        PUBLIC_KEY
-    );
-    setStatus("success");
-    setFormData({
-        name:"",
-        email:"",
-        service:"",
-        budget:"",
-        idea:"",
-    });
-} catch (err){
-    console.error("EmailJS Error:", err);
-    setStatus("error");
+    const validateform = () => {
+        const required = ["name", "email", "service", "idea"];
+        const newErrors = {};
+        required.foreach((f) => !formData[f].trim() && (newErrors[f] = "fill this field"));
+        if (formData.service !== "other" && !formData.budget.trim())
+            newErrors.budget = "fill this field";
+        setErrors(newErrors);
+        return !Object.keys(newErrors).length;
+    }
+    const handleSubmit = async (e) => {
+        e.preventDefoult();
+        if (!validateForm()) return;
+        setStatus("sending");
+    }
+    try {
+        await emailjs.send(
+            SEVICE_ID,
+            TEMPLATE_ID,
+            {
+                ...formData,
+                from_name: formData.name,
+                reply_to: formData.email,
+            },
+            PUBLIC_KEY
+        );
+        setStatus("success");
+        setFormData({
+            name: "",
+            email: "",
+            service: "",
+            budget: "",
+            idea: "",
+        });
+    } catch (err) {
+        console.error("EmailJS Error:", err);
+        setStatus("error");
+
+    }
 
 }
-    
-}
 
-    return(
-        <section id="contact" className="w-full min-h-screen relative bg-black overflow-hidden text-white py-20 px-6 md:px-20 flex flex-col
+return (
+    <section id="contact" className="w-full min-h-screen relative bg-black overflow-hidden text-white py-20 px-6 md:px-20 flex flex-col
         md:flex-row items-center gap-10
         ">
-<ParticlesBackground/>
-        </section>
-    )
+        <ParticlesBackground />
+        <div className="relative z-10 w-full flex flex-col mg:flex-row items-center gap-10 ">
+            <motion.div className="w-full md:w-1/2 flex justify-center 
+            initial={{opacity:0,x:-50}}
+            whileInView={{opacity:1,x:0}}
+            transition={{duration:0.6}}
+            ">
+                <motion.img src={Astra} alt="Contact"
+                className="w-72 md:w-140 rounded-2xl shadow-lg object-cover" 
+                animate={{y:[0,-10,0]}}
+                transition={{duration:2,repeat:Infinity, ease:"easeInOut"}}
+                
+                />
+            </motion.div> 
+            {/* right side */}
+            <motion.div className="w-full md:w-1/2 bg-white/5 p-8 rounded-2xl shadow-lg border border-white/10"
+            initial={{opacity:0 , x:50}}
+            whileInView={{opacity:1,x:0}}
+            transition={{duration:0.6}}
+            >
+     <h2 className="text-3xl font-bold mb-6">
+       Let's Work Together 
+     </h2>
+     <form className="flex flex-col gap-5" onChange={handleSubmit}>
+    <div className="flex flex-col">
+ <label className="mv-1">Your Name <span className="text-red-500"></span></label>
+ <input type="text"
+ name="name" 
+ placeholder="Your Name"
+ value={formData.name}
+ onChange={handleChange}
+ className={`p-3 rounded-md bg-white/10 border ${error.name ? "border-gray-500"} text-white focus:outline-none focus:border-blue-500`}
+ />
+ {errors.name && <p className="text-red-500 text-xs">{errors.name}</p>}
+ 
+    </div>
+    <div className="flex flex-col" >
+    <label className="mv-1">Your Email <span className="text-red-500"></span></label>
+ <input type="text"
+ name="email" 
+ placeholder="Your Email"
+ value={formData.email}
+ onChange={handleChange}
+ className={`p-3 rounded-md bg-white/10 border ${error.name ? "border-gray-500"} text-white focus:outline-none focus:border-blue-500`}
+  />
+ {errors.email && <p className="text-red-500 text-xs">{errors.email}</p>}
+
+    </div>
+
+     </form>
+            </motion.div>
+
+        </div>
+    </section>
+)
 }
