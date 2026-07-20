35568db7f32a9654068c8986ac76a1e054767ba1
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-07-20 16:57:52 +0530

    create a form section

--- My-portfolio/src/section/Contact.jsx
+++ My-portfolio/src/section/Contact.jsx
@@ -1,3 +1,4 @@
+import { useState } from "react";
 import ParticlesBackground from "../components/ParticlesBackground"
 
 
@@ -7,6 +8,13 @@ const SERVICE_ID = import.meta.env.VITE_SERVICE_ID;
 const TEMPLATE_ID = import.meta.env.VITE_TEMPLATE_ID;
 const PUBLIC_ID = import.meta.env.VITE_PUBLIC_KEY;
 export default function Contact(){
+    const [formdata , setformData]= useState({
+        name : "",
+        email :"";
+        service:"",
+        budget:"",
+        idea :"",
+    });
     return(
         <section id="contact" className="w-full min-h-screen relative bg-black overflow-hidden text-white py-20 px-6 md:px-20 flex flex-col
         md:flex-row items-center gap-10
