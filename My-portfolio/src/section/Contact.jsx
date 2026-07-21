23ab8832d64a1f2f062f7d250193c9f60530532c
Bhavnameshram <bhawanameshram1910@gmail.com>
2026-07-21 14:59:47 +0530

    creating button  define the trasition hover and apply amotion

--- My-portfolio/src/section/Contact.jsx
+++ My-portfolio/src/section/Contact.jsx
@@ -176,6 +176,14 @@ return (
                             {status === "sending" ? "sending...": status === "success"? "Message sent successfully ✅" : "Something went wrong ❌"}
                         </p>
                     )}
+                    <motion.button className=" bg-blue-600 hover:bg-blue-700 disabled:opacity-60 text-white py-3 rounded-md font-semibold transition "
+                    whileHover={{scale:1.05}}
+                    whileTap={{scale:0.95}}
+                    disabled={status === "sending"}
+                    type="submit"
+                    >
+      {status === "sending" ? "sending..." : "Send Message"}
+                    </motion.button>
                 </form>
             </motion.div>
 
